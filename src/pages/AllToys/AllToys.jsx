db8a4ed0fb6ce26c1471a0c86ac8f33761f1baf4
Mohammad Sakib <mohammadsakib7679@gmail.com>
2023-05-20 10:14:57 +0600

    search method in the all toys page

--- src/pages/AllToys/AllToys.jsx
+++ src/pages/AllToys/AllToys.jsx
@@ -3,6 +3,7 @@ import ToysCard from "./ToysCard";
 
 const AllToys = () => {
   const [toys, setToys] = useState([]);
+  const [searchText, setSearchText] = useState('');
   console.log(toys);
 
   useEffect(() => {
@@ -10,8 +11,41 @@ const AllToys = () => {
       .then((res) => res.json())
       .then((data) => setToys(data));
   }, []);
+  const handleSearch = () => {
+    fetch(`https://assignment11-toy-market-place-server.vercel.app/getJobsByText/${searchText}`)
+    .then((res) => res.json())
+    .then(data => {
+      setToys(data);
+    })
+  }
   return (
-    <div className="">
+    <div>
+      <div className="form-control my-8 w-1/4 ml-auto">
+        <div className="input-group">
+          <input
+          onChange={(e) => setSearchText(e.target.value)}
+            type="text"
+            placeholder="Search…"
+            className="input input-bordered"
+          />
+          <button onClick={handleSearch} className="btn btn-square">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 "
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
       <div className="overflow-x-auto w-full">
         <table className="table w-full">
           <thead>
@@ -29,12 +63,9 @@ const AllToys = () => {
             </tr>
           </thead>
           <tbody>
-          {
-                toys.map(toy => <ToysCard
-                 key={toy._id}
-                 toy={toy}
-                ></ToysCard>)
-            }
+            {toys.map((toy) => (
+              <ToysCard key={toy._id} toy={toy}></ToysCard>
+            ))}
           </tbody>
         </table>
       </div>
